8166d254fd356e17848c14165fe6c9ae8e9f79f8
IlyaChaban <54276624+IlyaChaban@users.noreply.github.com>
2020-10-22 14:54:33 +0200

    Add files via upload

--- trideni.cpp
+++ trideni.cpp
@@ -1,4 +1,5 @@
 #include<fstream>
+#include <iostream>
 #include "trideni.h"
 
 void nacti(std::string nazev, seznam_slov & jmena){
@@ -16,12 +17,26 @@ void nacti(std::string nazev, seznam_slov & jmena){
 
 
 void serad(seznam_slov & jmena){
-}
+	std::string tmp;
+	bool bylo_prohozeno;
+	do{
+		bylo_prohozeno=false;
+		for(int i=1;i<jmena.size();i++){
+			if(jmena[i].compare(jmena[i-1])<0){
+				tmp=jmena[i];
+				jmena[i]=jmena[i-1];
+				jmena[i-1]=tmp;
+				bylo_prohozeno=true;
+			}
+		}
+	}while(bylo_prohozeno);
+	}	
+
 
 
 void vypis(seznam_slov & jmena){
 	for (int i=0; i<jmena.size();i++){
-		std::cout<<jmena(i)<<std::cout;
+		std::cout << jmena[i] << std::endl;
 	} 
 }
 
@@ -29,10 +44,10 @@ void vypis(seznam_slov & jmena){
 bool zeptej_se_jestli_vypsat(){
 	char vypsat;
 	while(true){
-		std::cout<<"Chcete vypsat seznam"<< 
+		std::cout<<"Chcete vypsat seznam"<<std::endl;
 		std::cin>>vypsat;
-		if(vypsat =='A'|| vypsat =='a') return true;<<std::endl;
+		if(vypsat =='A'|| vypsat =='a') return true;
 		else if (vypsat == 'N' || vypsat =='n') return false;
-		else std::cout <<"Nerozumim.";		
+		else std::cout << "Nerozumim.";		
 	}
 }
